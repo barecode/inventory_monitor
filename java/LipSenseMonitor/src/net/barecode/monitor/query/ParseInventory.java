1ac55f13e00ed028fd26dfb7123266757f343de9
Michael C. Thompson <mcthomps@us.ibm.com>
2017-07-11 09:02:53 -0500

    Extremely ugly and fragile (but effective) parser for inventory page

---
+++ java/LipSenseMonitor/src/net/barecode/monitor/query/ParseInventory.java
@@ -0,0 +1,70 @@
+package net.barecode.monitor.query;
+
+import net.barecode.monitor.inventory.Inventory;
+import net.barecode.monitor.inventory.InventoryCategory;
+import net.barecode.monitor.inventory.InventoryItem;
+
+public class ParseInventory {
+
+	public Inventory parseInventory(String inventoryHTML) {
+		Inventory inventory = new Inventory();
+
+		parseCategory(inventoryHTML, inventory, "Liquid Lip Colors (0.25 oz)");
+		parseCategory(inventoryHTML, inventory, "Glosses (0.25 oz)");
+
+		return inventory;
+	}
+
+	/**
+	 * @param inventoryHTML
+	 * @param inventory
+	 */
+	private void parseCategory(String inventoryHTML, Inventory inventory, String categoryToFind) {
+		int colorsIndex = inventoryHTML.indexOf(categoryToFind);
+		int contianingElementEnd = inventoryHTML.lastIndexOf('>', colorsIndex);
+		int nextElementStart = inventoryHTML.indexOf('<', colorsIndex);
+		String categoryName = inventoryHTML.substring(contianingElementEnd+1, nextElementStart);
+		InventoryCategory category = new InventoryCategory(categoryName);
+		inventory.addCategory(category);
+
+		int nextCategoryIndex = inventoryHTML.indexOf("ProductCategoryDescription.aspx", colorsIndex);
+
+		int pos = colorsIndex;
+		int nextRowStart = inventoryHTML.indexOf("<tr>", pos);
+		int nextRowEnd = inventoryHTML.indexOf("</tr>", nextRowStart);
+		while (nextRowEnd < nextCategoryIndex) {
+			String itemHTML = inventoryHTML.substring(nextRowStart, nextRowEnd);
+			InventoryItem item = parseItem(itemHTML);
+			if (item != null) {
+				category.addItem(item);
+			}
+
+			pos = nextRowEnd+5;
+			nextRowStart = inventoryHTML.indexOf("<tr>", pos);
+			nextRowEnd = inventoryHTML.indexOf("</tr>", nextRowStart);
+		}
+	}
+
+	private InventoryItem parseItem(String itemHTML) {
+		int iNumIdx = itemHTML.indexOf("<a name='");
+		if (iNumIdx < 1) {
+			return null;
+		}
+		String iNumStr = itemHTML.substring(iNumIdx+9, iNumIdx+9+4);
+		System.out.println(iNumStr);
+		int itemNumber = Integer.parseInt(iNumStr);
+
+		int nameIdx = itemHTML.indexOf("#0000FF'>");
+		int nameIdxEnd = itemHTML.indexOf('<', nameIdx);
+		String name = itemHTML.substring(nameIdx+9, nameIdxEnd);
+		System.out.println(name);
+		
+		int stockIdx = itemHTML.indexOf("color=\"Red\" size=\"2\">", nameIdxEnd);
+		int stockIdxEnd = itemHTML.indexOf('<', stockIdx);
+		String stockStr = itemHTML.substring(stockIdx+"color=\"Red\" size=\"2\">".length(), stockIdxEnd);
+		System.out.println(stockStr);
+		boolean isInStock = stockStr.isEmpty();
+		// TODO Auto-generated method stub
+		return new InventoryItem(itemNumber, name, isInStock);
+	}
+}
--- java/LipSenseMonitor/src/net/barecode/monitor/rest/InventoryAPI.java
+++ java/LipSenseMonitor/src/net/barecode/monitor/rest/InventoryAPI.java
@@ -1,5 +1,10 @@
 package net.barecode.monitor.rest;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import net.barecode.monitor.inventory.Inventory;
 import net.barecode.monitor.inventory.InventoryCategory;
 import net.barecode.monitor.inventory.InventoryItem;
+import net.barecode.monitor.query.ParseInventory;
 
 /**
  * Read-only API to display the inventory of SeneGense at the time of the last
@@ -18,20 +24,29 @@ import net.barecode.monitor.inventory.InventoryItem;
 @Path("inventory")
 public class InventoryAPI {
 
-	private static Inventory inventory = new Inventory();
+	private static Inventory inventory;
 
 	static {
 		// Populate fake inventory
-		InventoryCategory c1 = new InventoryCategory("INDIVIDUAL - LipSense® Liquid Lip Colors (0.25 oz)");
-		c1.addItem(new InventoryItem(1360, "Apple Cider LipSense", true));
-		c1.addItem(new InventoryItem(1203, "Aussie Rose LipSense", false));
-		c1.addItem(new InventoryItem(1090, "Plum LipSense", false));
-		inventory.addCategory(c1);
+		ParseInventory parser = new ParseInventory();
+		File f = new File("inventory.html");
+		System.out.println(f.getAbsolutePath());
+		StringBuilder sb = new StringBuilder();
+		BufferedReader buf = null;
+		try {
+			FileInputStream fis = new FileInputStream(f);
+			buf = new BufferedReader(new InputStreamReader(fis));
+			String line = buf.readLine();
 
-		InventoryCategory c2 = new InventoryCategory("Glosses (0.25 oz)");
-		c2.addItem(new InventoryItem(1566, "Bougainvillea Gloss", false));
-		c2.addItem(new InventoryItem(1510, "Glossy Gloss", true));
-		inventory.addCategory(c2);
+			while (line != null) {
+				sb.append(line).append("\n");
+				line = buf.readLine();
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		String fileAsString = sb.toString();
+		inventory = parser.parseInventory(fileAsString);
 	}
 
 	@GET
